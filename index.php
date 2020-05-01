52fac4f39df3a011bcd90c4162371313770e16d8
Mohamed <Mohamed@scrapy.com>
2020-05-02 00:34:10 +0200

    Add some style & Normalize.css

--- index.php
+++ index.php
@@ -2,20 +2,30 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Al Shifa Hospital</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/DroidKufi-Regular.ttf">
- 
-    </head>
-    <body>
-        <h2>مرحبا بك ايها الزائر الكريم</h2>
-        
-        <script src="" async defer></script>
-    </body>
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Al Shifa Hospital</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/Normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="logo">
+            <img src="images/hospital-logo.jpg">
+            <h2>مستشفي الشفاء</h2>
+        </div>
+    </div>
+
+
+    <script src="" async defer></script>
+
+</body>
 </html>
